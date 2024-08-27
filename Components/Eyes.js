0046fd7cfcc313e53503c3f498ea410527e95078
unknown <roshananand2005@gmail.com>
2024-08-27 09:56:11 +0530

    eye animation integrated

---
+++ Components/Eyes.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const eyes = ({ eyeMoveX, eyeMoveY }) => {
+    return (
+        <>
+            <div className='border-2 border-white w-40 h-40 rounded-full overflow-hidden flex justify-center items-center relative'>
+
+                <div className='eyeLids'>
+                    <div className='topLid' style={{
+                        top: `${(eyeMoveY==-1000)?5:(eyeMoveY > 25) ? (eyeMoveY > 50) ? 50 - 90 : eyeMoveY - 95 : 24 - 90}%`,
+                    }}></div>
+                    <div className='bottomLid' style={{
+                        bottom: `${(eyeMoveY > 65) ? (eyeMoveY > 72) ? 60 - 72 : 60 - eyeMoveY : 10}%`,
+                    }}></div>
+                </div>
+
+                <div className='relative w-2/3 h-2/3'>
+                    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full w-1/4 h-1/4 bg-white text-black' style={{
+                        top: `${eyeMoveY}%`,
+                        left: `${eyeMoveX}%`
+                    }}>.</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default eyes
---
+++ app/page.js
@@ -0,0 +1,37 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import Eyes from "@/Components/Eyes"
+const page = () => {
+
+  const [mouseX, setmouseX] = useState(-20);
+  const [mouseY, setmouseY] = useState(-20);
+  const [eyeMoveX, seteyeMoveX] = useState();
+  const [eyeMoveY, seteyeMoveY] = useState(-1000);
+
+
+  useEffect(() => {
+    const setMouseMovement = (point) => {
+      setmouseX(point.x);
+      setmouseY(point.y);
+      seteyeMoveY(Math.floor(point.y * 100 / window.innerHeight))
+      seteyeMoveX(Math.floor(point.x * 100 / window.innerWidth))
+    }
+    window.addEventListener("mousemove", setMouseMovement);
+    return () => {
+      window.removeEventListener("mousemove", setMouseMovement)
+    }
+  })
+  return (
+    <>
+      <Eyes eyeMoveX={eyeMoveX} eyeMoveY={eyeMoveY} />
+      <Eyes eyeMoveX={eyeMoveX} eyeMoveY={eyeMoveY} />
+      <div className='mouth'></div>
+      <div className='bg-white w-5 h-5 rounded-full absolute z-50' style={{
+        top: `${mouseY}px`,
+        left: `${mouseX}px`
+      }}></div>
+    </>
+  )
+}
+
+export default page
